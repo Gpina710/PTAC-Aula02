4ad10456e9cafa404b705fffee42a73f3c531b7a
Gpina710 <160530123+Gpina710@users.noreply.github.com>
2024-03-01 16:05:31 -0400

    aula03
    ity

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
+import Header from "./components/Header";
+
 export default function App() {
+return(
+  <div>
+    <Header/>
+    <main>
 
-  const nome = "João";
-  const sobreNome = "Santos";
-
-  return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-    </div>  
-  );
-
-
+    </main>
+  </div>
+)
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <h1>Título!</h1>
+        </footer>
+    );
+}
