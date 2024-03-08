e07f15030b87f3a8edc561bfc08cb6d323a2c932
Gpina710 <160530123+Gpina710@users.noreply.github.com>
2024-03-08 16:09:37 -0400

    aula4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
+import "./globals.css";
 import Footer from "./components/Footer";
 export default function App() {
 return(
-  <div>
+  <div> 
     <Header/>
     <main>
 
-    </main>
+    </main> 
+    <Footer/>
   </div>
 )
 }
